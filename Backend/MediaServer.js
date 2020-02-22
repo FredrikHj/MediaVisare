7ea68f11f12f2df1c7c5769e33406b98163c5ed2
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2020-02-22 15:41:14 +0100

    Backup

---
+++ Backend/MediaServer.js
@@ -0,0 +1,44 @@
+// Creates a Express server in Node JS and use diff... modules    
+const express = require('express');
+const app = express();
+let cors = require('cors');
+
+// Module for handle the user logins
+//let jwt = require('jsonwebtoken');
+const fileSystem = require('fs');
+
+const path = require('path');
+const directoryPath = path.join('Images', '');
+
+
+//Config for the backend
+const backConfig = require('./backConfig.json');
+
+// The server information
+const port = process.env.PORT || backConfig.serverPort;
+
+// MYSQL module for connection
+//var mysql = require('mysql');
+app.listen(port, () => console.log(`MediaVisare is listening on port ${port}!`));
+
+app.get('/ReqMedia', cors(), (req, res) => {
+    let imagesList = [];
+/*     console.log('fdb');
+    fileSystem.writeFile('../../../../R/regedUser.json', JSON.stringify(regedUser //debugging , null, 2
+        ), function(err) {console.log(err);
+    }); */
+    fileSystem.readdir(directoryPath, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        //listing all files using forEach
+        files.forEach(function (file) {
+            // Do whatever you want to do with the file
+            console.log(file); 
+            imagesList.push(file);
+        });
+        res.status(200).send(imagesList);
+    });
+
+});
--- src/Components/Structure/HomeImages.js
+++ src/Components/Structure/HomeImages.js
@@ -1,15 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 //import { BrowserRouter as Router, Route, Link, Redirect} from "react-router-dom";
 import { StyleHomeImages } from '../Style/StyleHomeImages';
 
 export let HomeImages = () => {
- 
+  const [ fileList, setFileList ] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:3001/ReqMedia').then(response => {
+      if (fileList.length === 0) setFileList(response.data);
+      console.log("TCL: HomeImages -> response", response)
+      
+    
+    }).catch(error => {
+      ////console.log(error.response);
+    });
+  },[fileList] );
   return (
     <StyleHomeImages.container>
       
       <aside id="appBody__patch">
-      Bilder -
-      Mapp och filer kommer här
+      Bilder:
+        <section>
+          {(fileList.length !== 0)
+            ? fileList.map((item, index) => {
+                console.log(item);
+                return(
+                  <div key={ index }><img src={'Images/' + item} alt="erge"/></div>
+                );
+              })
+            : 'inget inläst'
+          }
+        </section>
+      'Mapp och filer kommer här'
 
       </aside>
       <main id="appBody__mainContent">
