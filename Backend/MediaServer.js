ab2f1b69f7606b58454bb389d7f1ab64859f8137
Your Name <you@example.com>
2020-12-18 18:15:27 +0100

    Working with prentation of the immages and the individual icons size

--- Backend/MediaServer.js
+++ Backend/MediaServer.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 
-
 let cors = require('cors');
 const mediaRootPath = 'u:\\testMedia\\';
 app.use(express.static(mediaRootPath));
--- src/Components/Structure/ShowFiles.js
+++ src/Components/Structure/ShowFiles.js
@@ -6,29 +6,36 @@ import { MappFilesIconStyle } from '../Style/MappFilesIconsStyle';
 
 // Import inportant components for the specific page
 import { BackendURL } from'../Data/BackendURL';
+let imgIndex = -1;
 
 export let ShowFiles = (props) => {
     const [ imgPxSizeRefArr, setImgPxSizeRefArr ] = useState([]);
-
-    console.log("🚀 ~ file: ShowFiles.js ~ line 12 ~ ShowFiles ~ refsImgSizeInPx", imgPxSizeRefArr)
-
     const { fileList } = props;
-    console.log("🚀 ~ file: ShowFiles.js ~ line 12 ~ ShowFiles ~ fileList", fileList)
+
+    useEffect(() => {
+
+    },[])
     
     let saveImgPxSize = (e) => {
-        console.log("🚀 ~ file: ShowFiles.js ~ line 21 ~ saveImgPxSize ~ e", e)
+        let index = imgIndex++;
+        let targetImg = e.target;
+        console.log("🚀 ~ file: ShowFiles.js ~ line 12 ~ ShowFiles ~ fileList", fileList)
         
-    }
+        //if(imgPxSizeRefArr.length <= index) imgPxSizeRefArr[index].push(React.createRef()); 
+        
+        // Make the icon --> size object in the fileList and its two keys as React.createRef()
+        /* fileList[index].icon.size.heigth = React.createRef();
+        fileList[index].icon.size.width = React.createRef();
+ */    }
 
     return( 
         <MappFilesIconStyle.fileIconContainer>
             {fileList !== {} && fileList !== undefined &&
                 fileList.map((item, index) => {
-                    //if(imgPxSizeRefArr[index].length <= index) imgPxSizeRefArr[index].push(React.createRef()); 
                     const sourcePath = BackendURL + item.path + item.name;
                     return(
                         <>
-                            <MappFilesIconStyle.mediaIconImg key={ index } src={ sourcePath } ref={ imgPxSizeRefArr[index]} onChange={ saveImgPxSize }></MappFilesIconStyle.mediaIconImg>
+                            <MappFilesIconStyle.mediaIconImg key={ index } src={ sourcePath } onLoad={ saveImgPxSize }></MappFilesIconStyle.mediaIconImg>
                         </>
                     );
                 })            
